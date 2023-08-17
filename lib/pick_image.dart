4a517b93ba91eb366ec8a090f58f6483d2b4f07a
ae22niidome <ae22niidome@g.kumamoto-nct.ac.jp>
2023-08-17 10:03:47 +0900

    [add] Camera fanction of imagepicker is OK.

---
+++ lib/pick_image.dart
@@ -0,0 +1,89 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? image;
+  // 画像をギャラリーから選ぶ関数
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      // 画像がnullの場合戻る
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  // カメラを使う関数
+  Future pickImageC() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      // 画像がnullの場合戻る
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Image Picker Example"),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            MaterialButton(
+              color: Colors.blue,
+              child: const Text(
+                "Pick Image from Garary",
+                style: TextStyle(
+                    color: Colors.white70, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                pickImage();
+              },
+            ),
+            MaterialButton(
+              color: Colors.blue,
+              child: const Text(
+                "Pick Image from Camera",
+                style: TextStyle(
+                    color: Colors.white70, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                pickImageC();
+              },
+            ),
+            SizedBox(height: 20),
+            // 画像がないと文字が表示される
+            image != null ? Image.file(image!) : Text("No image selected")
+          ],
+        )) // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
